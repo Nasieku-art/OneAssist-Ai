7b986d18f9733f1f0d9030907b6a71ef163862b0
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-21 10:02:55 +0300

    Added a context for useprofile

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,9 +1,14 @@
-const getInitials=(fullName)
+const getInitials=(fullName ) =>{
+    if(!fullName) return "U"
+}
+const namePart =fullName.trim().split()
 
 function UserProfile(){
     return(
         
+<div>
 
+</div>
     )
 }
 export default UserProfile;

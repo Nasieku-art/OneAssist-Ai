2e1209d865138f4d9e17f6467484495937af9079
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-23 17:03:18 +0300

    Added a new styling on signup

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sign from "../Components/Sign";
+
+function SignUp() {
+    return(
+        <div className="min-h-screen bg-white">
+            <Sign />
+        </div>
+    );
+}
+
+export default SignUp;

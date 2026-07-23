f19332cabc420d4051c4e01ec6ec124a53135160
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-23 16:58:36 +0300

    Added new context in dash

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import Dash from "../Components/Dash";
 
-function Dashboard(){
-  return(
-   <div>
-<Dash/>
-   </div>
-  )
+function Dashboard() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <Dash />
+    </div>
+  );
 }
+
 export default Dashboard;

a69b500e0b342da9f951b97a7c6ba804d6691a67
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-23 16:56:31 +0300

    Added new content on useprofile

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,26 +1,28 @@
-const getInitials=(fullName ) =>{
-    if(!fullName) return "U"
+const getInitials = (fullName) => {
+  if (!fullName) return "U";
 
- const nameParts = fullName.trim().split(/\s+/)
- if (nameParts.length === 0) return 'U';
+  const nameParts = fullName.trim().split(/\s+/);
+  if (nameParts.length === 0) return 'U';
   if (nameParts.length === 1) {
      return nameParts[0][0].toUpperCase();
   }
   const firstInitial = nameParts[0][0];
   const lastInitial = nameParts[nameParts.length - 1][0];
   
-  return $(firstInitial)$(lastInitial).toUpperCase();
+  
+  return `${firstInitial}${lastInitial}`.toUpperCase();
 };
 
-function UserProfile(){
+function UserProfile({ fullName, onClick }) {
     return(
-        
-<div  onClick={onClick}
-      className="flex items-center justify-center w-10 h-10 font-semibold text-white bg-teal-900 rounded-full shadow-sm cursor-pointer select-none hover:bg-indigo-700 transition-colors"
-      title={fullName || 'User Profile'}>
-    {getInitials(fullName)}
-
-</div>
-    )
+      <div 
+        onClick={onClick}
+        className="flex items-center justify-center w-10 h-10 font-semibold text-white bg-teal-900 rounded-full shadow-sm cursor-pointer select-none hover:bg-teal-700 transition-colors"
+        title={fullName || 'User Profile'}
+      >
+        {getInitials(fullName)}
+      </div>
+    );
 }
+
 export default UserProfile;

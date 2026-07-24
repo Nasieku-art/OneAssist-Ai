6dad39f2250cdbef7be957852859540087f1072d
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-07-24 09:39:01 +0300

    removed conatiner

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="container mx-auto">
+    <div>
       <footer className="bg-slate-800 text-slate-300 py-6 text-center text-sm border-t border-slate-700">
+        <div className='container mx-auto'>
         <p>
           © {new Date().getFullYear()} OneAssist AI
           Technology that adapts people, NOT people adapting to technology.
         </p>
+        </div>
       </footer>
     </div>
   );
